1a52dfe58fefa2442ae655aafb40a713c39338db
Pradhamlr <pradham.lr@gmail.com>
2025-09-30 09:58:21 +0530

    Signup Routes

--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -1,1 +1,8 @@
 // Express routes: Auth
+const express = require('express');
+const router = express.Router();
+const { signup }= require('../controllers/authController.js');
+
+router.post("/signup", signup);
+
+module.exports = router;
