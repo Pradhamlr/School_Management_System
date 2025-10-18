c266ef55e81393897c2c133e1a46593a0304aeb5
Pradhamlr <pradham.lr@gmail.com>
2025-10-18 22:37:17 +0530

    Routes

--- Backend/src/routes/analyticsRoutes.js
+++ Backend/src/routes/analyticsRoutes.js
@@ -1,1 +1,8 @@
-// Express routes: Analytics
+const express = require('express');
+const router = express.Router();
+const { getAnalytics } = require('../controllers/analyticsController');
+const authorize = require('../middlewares/roleMiddleware');
+
+router.get('/', authorize('ADMIN'), getAnalytics);
+
+module.exports = router;
